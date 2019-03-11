d3c7d420ce750d4b71f8b87f1cefcbbef2f3ca4c
John Patton <jpatton@usgs.gov>
2019-03-11 16:26:07 -0600

    in progress commit of code style and javadoc reformating

--- src/main/java/gov/usgs/locator/AuthorType.java
+++ src/main/java/gov/usgs/locator/AuthorType.java
@@ -1,64 +1,89 @@
 package gov.usgs.locator;
 
 /**
- * Author types in the Hydra Locator interface are 
- * specified by integer constants from 1 to 4 with 
- * the meanings defined below.
+ * AuthorType is an enumeration that defines the possible pick author types and 
+ * default pick affinities for the Locator.
  * 
  * @author Ray Buland
- *
  */
 public enum AuthorType {
-	/**
-	 * All picks should fall into one of the four categories below.  
-	 * Unknown handles bad input.
-	 */
-	UNKNOWN (0.0d),				// Just in case
-	/**
-	 * An automatic pick from outside the NEIC.
-	 */
-	CONTRIB_AUTO (1.0d),	// An automatic pick from outside the NEIC
-	/**
-	 * An automatic pick from the NEIC.
-	 */
-	LOCAL_AUTO (1.0d),		// An automatic pick from the NEIC
-	/**
-	 * An analyst pick from outside the NEIC.
-	 */
-	CONTRIB_HUMAN (1.5d),	// An analyst pick from outside the NEIC
-	/**
-	 * An analyst pick from the NEIC.
-	 */
-	LOCAL_HUMAN (3.0d);		// An analyst pick from the NEIC
-	
-	private final double defAffinity;	// Default affinity based on type
-	
-	/**
-	 * The constructor just sets up the default affinities.
-	 * 
-	 * @param defAffinity Default affinity
-	 */
-	AuthorType(double defAffinity) {
-		this.defAffinity = defAffinity;
-	}
-	
-	/**
-	 * Get the default affinity.
-	 * 
-	 * @return Default affinity
-	 */
-	public double defAffinity() {
-		return defAffinity;
-	}
-	
-	/**
-	 * Apply the default to a user supplied affinity.
-	 * 
-	 * @param userAffinity User supplied affinity
-	 * @return Final affinity
-	 */
-	public double affinity(double userAffinity) {
-		if(userAffinity > 0d) return userAffinity;
-		else return defAffinity;
-	}
+  /**
+   * This enumeration defines an author type for unknown picks for handing bad
+   * input, and initializes the default affinity for those picks to 0.0.
+   */
+  UNKNOWN(0.0d),
+
+  /**
+   * This enumeration defines an author type for automatically generated picks 
+   * made outside the NEIC, and initializes the default affinity for those picks 
+   * to 1.0.
+   */
+  CONTRIB_AUTO(1.0d),
+
+  /**
+   * This enumeration defines an author type for automatically generated picks 
+   * made within the NEIC, and initializes the default affinity for those picks 
+   * to 1.0.
+   */
+  LOCAL_AUTO(1.0d),
+
+  /**
+   * This enumeration defines an author type for analyst picks made outside the
+   * NEIC, and initializes the default affinity for those picks to 1.5.
+   */
+  CONTRIB_HUMAN(1.5d),
+
+  /**
+   * This enumeration defines an author type for analyst picks made within the
+   * NEIC, and initializes the default affinity for those picks to 3.0.
+   */
+  LOCAL_HUMAN(3.0d);
+  
+  /**
+   * A double containing the default affinity value for an enumeration.
+   **/ 
+  private final double defaultAffinity;
+  
+  /**
+   * The AuthorType constructor. This constructor initializes the default 
+   * affinity for this author type enumeration to the provided value.
+   * 
+   * @param defaultAffinity A double containing the default affinity to use for
+   *                        this type.
+   */
+  AuthorType(double defaultAffinity) {
+    this.defaultAffinity = defaultAffinity;
+  }
+  
+  /**
+   * This function gets the default affinity for this author type.
+   * 
+   * @return The default affinity as a double.
+   */
+  public double getDefaultAffinity() {
+    return defaultAffinity;
+  }
+  
+  /**
+   * This function gets the author affinity based on a user supplied affinity. 
+   * If the user affinity is less than or equal zero, the default affinity is
+   * provided
+   * 
+   * @param userAffinity A double value containign the affinity provided by the 
+   *                     user. If greater than zero, this affinity will be used 
+   *                     instead of the default affinity.
+   * @return The final affinity as a double. This value will be equal to the 
+   *         provided user affinity if the user affinity is greater than zero. 
+   *         Otherwise the default affinity is returned.
+   */
+  public double affinity(double userAffinity) {
+    // check for a positive, nonzero affinity
+    if (userAffinity > 0d) {
+      // use the provided affinity
+      return userAffinity;
+    } else {
+      // use the default affinity
+      return defaultAffinity;
+    }
+  }
 }
--- src/main/java/gov/usgs/locator/AuxLocRef.java
+++ src/main/java/gov/usgs/locator/AuxLocRef.java
@@ -1,5 +1,7 @@
 package gov.usgs.locator;
 
+import gov.usgs.traveltime.FileChanged;
+
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -13,25 +15,31 @@ import java.nio.IntBuffer;
 import java.nio.channels.FileLock;
 import java.util.Scanner;
 
-import gov.usgs.traveltime.FileChanged;
-
 /**
- * Read in auxiliary data to support the Locator.  Note that this 
- * data can never change and will be common to all events subsequently 
- * located.
+ * The AuxLocRef class manages auxiliary data files that support the Locator 
+ * such as the continental craton boundries and earthquake statistics by 
+ * geographic location files.  Note that this data is static (does not change
+ * during a location run) and will be common to all events subsequently located.
  * 
  * @author Ray Buland
  *
  */
 public class AuxLocRef {
-	final Cratons cratons;			// Continental craton boundaries
-	final ZoneStats zoneStats;	// Earthquake statistics by geographic location
-	int noYears = -1;
-	Scanner scan;
-	// Set up serialization.
-	String serName = "locaux.ser";					// Serialized file name
-	String[] fileNames = {"cratons.txt", "zonekey.dat", 
-			"zonestat.dat"};										// Raw input file names
+  /** 
+   * A Cratons object containing the continental craton boundaries.
+   */
+  private final Cratons cratons;
+
+  /**
+   * A ZoneStats object containing earthquake statistics by geographic location.
+   **/ 
+  private final ZoneStats zoneStats;
+
+  /** 
+   * An integer containing the number of years read from the earthquake 
+   * statistics zonestats file.
+   */
+  private int numberOfYears = -1;
   
   /**
 	 * Default path for model files.
@@ -42,6 +50,11 @@ public class AuxLocRef {
 	 * Path for model files.
 	 */
 	public String modelPath;      
+
+  /**
+   * An array of String objets containing the raw input model file names.
+   */
+  private String[] modelFileNames = {"cratons.txt", "zonekey.dat", "zonestat.dat"};
 
 	/**
 	 * Read the cratons and zone statistics files and make the data available 
@@ -71,192 +84,186 @@ public class AuxLocRef {
       this.modelPath = modelPath;
     }
 
-		// Create absolute path names.
-		absNames = new String[fileNames.length];
-		for(int j=0; j<fileNames.length; j++) {
-			absNames[j] = modelPath+fileNames[j];
-		}
-		
-		// If any of the raw input files have changed, regenerate the 
-		// serialized file.
-		if(FileChanged.isChanged(modelPath+serName, absNames)) {
-//		time = System.currentTimeMillis();
-			// Open and read the cratons file.
-			inCratons = new BufferedInputStream(new FileInputStream(absNames[0]));
-			scan = new Scanner(inCratons);
-			cratons = new Cratons();
-			while(scan.hasNext()) {
-				readCraton();
-			}
-			scan.close();
-			inCratons.close();
-			
-			// Open and read the zone key file.
-			inZones = new RandomAccessFile(absNames[1], "r");
-			zoneKeys = readZoneKeys(inZones);
-			zoneStats = new ZoneStats(zoneKeys);
-			inZones.close();
-			
-			// Open and read the zone statistics file.
-			inZones = new RandomAccessFile(absNames[2], "r");
-			stats = readZoneStats(inZones);
-			zoneStats.addStats(noYears, stats);
-			inZones.close();
-			
-			// Write out the serialized file.
-			serOut = new FileOutputStream(modelPath+serName);
-			objOut = new ObjectOutputStream(serOut);
-			// Wait for an exclusive lock for writing.
-			lock = serOut.getChannel().lock();
-			if(LocUtil.deBugLevel > 0) {
-				System.out.println("AuxLocRef write lock: valid = "+lock.isValid()+
-						" shared = "+lock.isShared());
-			}
-			/*
-			 * The auxiliary data can be read and written very quickly, so for persistent 
-			 * applications such as the travel time or location server, serialization is 
-			 * not necessary.  However, if the travel times are needed for applications 
-			 * that start and stop frequently, the serialization should save some set up 
-			 * time.
-			 */
-			objOut.writeObject(cratons);
-			objOut.writeObject(zoneStats);
-			if(lock.isValid()) lock.release();
-			objOut.close();
-			serOut.close();
-//		System.out.format("Serialize loc aux out: %5.3f\n", 
-//			0.001*(System.currentTimeMillis()-time));
-		} else {
-			// Read in the serialized file.
-//		time = System.currentTimeMillis();
-			serIn = new FileInputStream(modelPath+serName);
-			objIn = new ObjectInputStream(serIn);
-			// Wait for a shared lock for reading.
-			lock = serIn.getChannel().lock(0, Long.MAX_VALUE, true);
-			if(LocUtil.deBugLevel > 0) {
-				System.out.println("AuxLocRef read lock: valid = "+lock.isValid()+
-						" shared = "+lock.isShared());
-			}
-			cratons = (Cratons)objIn.readObject();
-			zoneStats = (ZoneStats)objIn.readObject();
-			if(lock.isValid()) lock.release();
-			objIn.close();
-			serIn.close();
-//		System.out.format("Serialize loc aux in: %5.3f\n", 
-//			0.001*(System.currentTimeMillis()-time));
-		}
-	}
-	
-	/**
-	 * Read the craton file.
-	 */
-	private void readCraton() {
-		String region;
-		double[] lats, lons;
-		
-		region = scan.next();
-		while(!scan.hasNextInt()) {
-			region = region+" "+scan.next();
-		}
-		int len = scan.nextInt();
-		lats = new double[len];
-		lons = new double[len];
-		
-		for(int j=0; j<len; j++) {
-			lats[j] = scan.nextDouble();
-			lons[j] = scan.nextDouble();
-		}
-		cratons.add(region, lats, lons);
-	}
-	
-	/**
-	 * Read the zone keys file.  Because the earthquake statistics are sparse 
-	 * with respect to Marsden squares, it was convenient to index them 
-	 * through a separate file.  Note that the zone keys array is organized by 
-	 * longitude (Greenwich to Greenwich heading east) and co-latitude (north 
-	 * pole to south pole going south).
-	 * 
-	 * @param inZones File handle for the zone keys file
-	 * @return Zone keys
-	 * @throws IOException On any read error
-	 */
-	private int[][] readZoneKeys(RandomAccessFile inZones) throws IOException {
-		int length;
-		byte[] byteArray;
-		int[][] zoneKeys;
-		ByteBuffer byteBuf;
-		IntBuffer intBuf;
-		
-		// Read the file.
-		length = (int)inZones.length();
-		byteArray = new byte[length];
-		inZones.read(byteArray);
-		byteBuf = ByteBuffer.wrap(byteArray);
-		byteBuf.order(ByteOrder.LITTLE_ENDIAN);
-		intBuf = byteBuf.asIntBuffer();
-		
-		// Create the zoneKeys array.
-		zoneKeys = new int[360][180];
-		int k=0;
-		for(int i=0; i<180; i++) {
-			for(int j=0; j<360; j++) {
-				// Note that the key is off by 2 because of Fortran indexing and the 
-				// special first record in the statistics file.
-				zoneKeys[j][i] = intBuf.get(k++)-2;
-			}
-		}
-		return zoneKeys;
-	}
-	
-	/**
-	 * Read in the zone statistics file.  Note that only the small part of the 
-	 * statistics relevant to the Locator is currently retained.
-	 * 
-	 * @param inZones File handle for the zone statistics file
-	 * @return Zone statistics
-	 * @throws IOException On any read error
-	 */
-	private ZoneStat[] readZoneStats(RandomAccessFile inZones) throws IOException {
-		@SuppressWarnings("unused")
-		int length, ndeg, dpmode;
-		@SuppressWarnings("unused")
-		float peryr, maxmag, minmag, pctfre, pctge;
-		double meanDepth, minDepth, maxDepth;
-		byte[] byteArray;
-		ByteBuffer byteBuf;
-		ZoneStat[] stats;
-		
-		// Read the file.
-		length = (int)inZones.length();
-		byteArray = new byte[length];
-		inZones.read(byteArray);
-		byteBuf = ByteBuffer.wrap(byteArray);
-		byteBuf.order(ByteOrder.LITTLE_ENDIAN);
-		noYears = byteBuf.getInt();
-//	System.out.println("ZoneStats years = "+noYears);
-		byteBuf.position(40);
-		
-		// Create the zoneStats array.
-		stats = new ZoneStat[zoneStats.size()];
-		for(int j=0; j<stats.length; j++) {
-			ndeg = byteBuf.getInt();
-			peryr = byteBuf.getFloat();
-			maxmag = byteBuf.getFloat();
-			minmag = byteBuf.getFloat();
-			minDepth = byteBuf.getFloat();
-			maxDepth = byteBuf.getFloat();
-			pctfre = byteBuf.getFloat();
-			meanDepth = byteBuf.getFloat();
-			dpmode = byteBuf.getInt();
-			pctge = byteBuf.getFloat();
-			if(pctfre > 0. && minDepth < 900d) {
-				stats[j] = new ZoneStat(meanDepth, minDepth, maxDepth);
-	//		System.out.format("\t%3d %4.1f %4.1f %4.1f %5.1f %5.1f %4.1f %5.1f %3d %4.1f\n", 
-	//				ndeg, peryr, maxmag, minmag, minDepth, maxDepth, pctfre, meanDepth, dpmode, 
-	//				pctge);
-			}
-			else stats[j] = null;
-		}
-		return stats;
-	}
+    // Create absolute path names.
+    absNames = new String[modelFileNames.length];
+    for(int j=0; j<modelFileNames.length; j++) {
+      absNames[j] = modelPath+modelFileNames[j];
+    }
+    
+    // If any of the raw input files have changed, regenerate the 
+    // serialized file.
+    if(FileChanged.isChanged(modelPath+serializedFileName, absNames)) {
+      // Open and read the cratons file.
+      inCratons = new BufferedInputStream(new FileInputStream(absNames[0]));
+      scan = new Scanner(inCratons);
+      cratons = new Cratons();
+      while(scan.hasNext()) {
+        readCraton();
+      }
+      scan.close();
+      inCratons.close();
+      
+      // Open and read the zone key file.
+      inZones = new RandomAccessFile(absNames[1], "r");
+      zoneKeys = readZoneKeys(inZones);
+      zoneStats = new ZoneStats(zoneKeys);
+      inZones.close();
+      
+      // Open and read the zone statistics file.
+      inZones = new RandomAccessFile(absNames[2], "r");
+      stats = readZoneStats(inZones);
+      zoneStats.addStats(numberOfYears, stats);
+      inZones.close();
+      
+      // Write out the serialized file.
+      serOut = new FileOutputStream(modelPath+serializedFileName);
+      objOut = new ObjectOutputStream(serOut);
+      // Wait for an exclusive lock for writing.
+      lock = serOut.getChannel().lock();
+      if(LocUtil.deBugLevel > 0) {
+        System.out.println("AuxLocRef write lock: valid = "+lock.isValid()+
+            " shared = "+lock.isShared());
+      }
+      /*
+       * The auxiliary data can be read and written very quickly, so for persistent 
+       * applications such as the travel time or location server, serialization is 
+       * not necessary.  However, if the travel times are needed for applications 
+       * that start and stop frequently, the serialization should save some set up 
+       * time.
+       */
+      objOut.writeObject(cratons);
+      objOut.writeObject(zoneStats);
+      if(lock.isValid()) lock.release();
+      objOut.close();
+      serOut.close();
+    } else {
+      // Read in the serialized file.
+      serIn = new FileInputStream(modelPath+serializedFileName);
+      objIn = new ObjectInputStream(serIn);
+      // Wait for a shared lock for reading.
+      lock = serIn.getChannel().lock(0, Long.MAX_VALUE, true);
+      if(LocUtil.deBugLevel > 0) {
+        System.out.println("AuxLocRef read lock: valid = "+lock.isValid()+
+            " shared = "+lock.isShared());
+      }
+      cratons = (Cratons)objIn.readObject();
+      zoneStats = (ZoneStats)objIn.readObject();
+      if(lock.isValid()) lock.release();
+      objIn.close();
+      serIn.close();
+    }
+  }
+  
+  /**
+   * Read the craton file.
+   */
+  private void readCraton() {
+    String region;
+    double[] lats, lons;
+    
+    region = scan.next();
+    while(!scan.hasNextInt()) {
+      region = region+" "+scan.next();
+    }
+    int len = scan.nextInt();
+    lats = new double[len];
+    lons = new double[len];
+    
+    for(int j=0; j<len; j++) {
+      lats[j] = scan.nextDouble();
+      lons[j] = scan.nextDouble();
+    }
+    cratons.add(region, lats, lons);
+  }
+  
+  /**
+   * Read the zone keys file.  Because the earthquake statistics are sparse 
+   * with respect to Marsden squares, it was convenient to index them 
+   * through a separate file.  Note that the zone keys array is organized by 
+   * longitude (Greenwich to Greenwich heading east) and co-latitude (north 
+   * pole to south pole going south).
+   * 
+   * @param inZones File handle for the zone keys file
+   * @return Zone keys
+   * @throws IOException On any read error
+   */
+  private int[][] readZoneKeys(RandomAccessFile inZones) throws IOException {
+    int length;
+    byte[] byteArray;
+    int[][] zoneKeys;
+    ByteBuffer byteBuf;
+    IntBuffer intBuf;
+    
+    // Read the file.
+    length = (int)inZones.length();
+    byteArray = new byte[length];
+    inZones.read(byteArray);
+    byteBuf = ByteBuffer.wrap(byteArray);
+    byteBuf.order(ByteOrder.LITTLE_ENDIAN);
+    intBuf = byteBuf.asIntBuffer();
+    
+    // Create the zoneKeys array.
+    zoneKeys = new int[360][180];
+    int k=0;
+    for(int i=0; i<180; i++) {
+      for(int j=0; j<360; j++) {
+        // Note that the key is off by 2 because of Fortran indexing and the 
+        // special first record in the statistics file.
+        zoneKeys[j][i] = intBuf.get(k++)-2;
+      }
+    }
+    return zoneKeys;
+  }
+  
+  /**
+   * Read in the zone statistics file.  Note that only the small part of the 
+   * statistics relevant to the Locator is currently retained.
+   * 
+   * @param inZones File handle for the zone statistics file
+   * @return Zone statistics
+   * @throws IOException On any read error
+   */
+  private ZoneStat[] readZoneStats(RandomAccessFile inZones) throws IOException {
+    @SuppressWarnings("unused")
+    int length, ndeg, dpmode;
+    @SuppressWarnings("unused")
+    float peryr, maxmag, minmag, pctfre, pctge;
+    double meanDepth, minDepth, maxDepth;
+    byte[] byteArray;
+    ByteBuffer byteBuf;
+    ZoneStat[] stats;
+    
+    // Read the file.
+    length = (int)inZones.length();
+    byteArray = new byte[length];
+    inZones.read(byteArray);
+    byteBuf = ByteBuffer.wrap(byteArray);
+    byteBuf.order(ByteOrder.LITTLE_ENDIAN);
+    numberOfYears = byteBuf.getInt();
+//  System.out.println("ZoneStats years = "+numberOfYears);
+    byteBuf.position(40);
+    
+    // Create the zoneStats array.
+    stats = new ZoneStat[zoneStats.size()];
+    for(int j=0; j<stats.length; j++) {
+      ndeg = byteBuf.getInt();
+      peryr = byteBuf.getFloat();
+      maxmag = byteBuf.getFloat();
+      minmag = byteBuf.getFloat();
+      minDepth = byteBuf.getFloat();
+      maxDepth = byteBuf.getFloat();
+      pctfre = byteBuf.getFloat();
+      meanDepth = byteBuf.getFloat();
+      dpmode = byteBuf.getInt();
+      pctge = byteBuf.getFloat();
+      if(pctfre > 0. && minDepth < 900d) {
+        stats[j] = new ZoneStat(meanDepth, minDepth, maxDepth);
+  //    System.out.format("\t%3d %4.1f %4.1f %4.1f %5.1f %5.1f %4.1f %5.1f %3d %4.1f\n", 
+  //        ndeg, peryr, maxmag, minmag, minDepth, maxDepth, pctfre, meanDepth, dpmode, 
+  //        pctge);
+      }
+      else stats[j] = null;
+    }
+    return stats;
+  }
 }
