47d415485c091153f1024992f998a0c56e7e8d77
John Patton <jpatton@usgs.gov>
2019-04-24 12:58:00 -0600

    more codacy fixes

--- src/test/java/gov/usgs/locator/LocInputTest.java
+++ src/test/java/gov/usgs/locator/LocInputTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.util.Date;
-import org.json.simple.parser.ParseException;
 import org.junit.Test;
 
 /**
