55f357ebb1ff475e016424611c1ab96649813f78
Jeremy Fee <jmfee@usgs.gov>
2019-06-23 18:23:04 -0600

    Comment out redirect for index page

--- src/main/java/gov/usgs/locatorservice/LocatorController.java
+++ src/main/java/gov/usgs/locatorservice/LocatorController.java
@@ -4,14 +4,10 @@ import gov.usgs.locator.LocService;
 import gov.usgs.processingformats.LocationException;
 import gov.usgs.processingformats.LocationResult;
 import io.micronaut.context.annotation.Value;
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
-import io.micronaut.http.annotation.Produces;
-import java.net.URI;
 
 @Controller("${locator.controller.path:/locator}")
 public class LocatorController {
@@ -19,11 +15,11 @@ public class LocatorController {
   @Value("${locator.model.path:./build/models/}")
   String modelPath;
 
-  @Get("/")
-  @Produces(MediaType.TEXT_HTML)
-  HttpResponse index() {
-    return HttpResponse.redirect(URI.create("/locator/index.html"));
-  }
+  // @Get("/")
+  // @Produces(MediaType.TEXT_HTML)
+  // HttpResponse index() {
+  //   return HttpResponse.redirect(URI.create("/locator/index.html"));
+  // }
 
   @Post(uri = "/locate", consumes = MediaType.APPLICATION_JSON)
   public LocatorResponse getLocation(@Body LocatorRequest request) throws LocationException {
