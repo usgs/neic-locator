03ec84d09019a5d11198dab46f8cba9c44d587c0
William L Yeck <wyeck@usgs.gov>
2020-07-16 12:54:36 -0600

    Changed it so when a pick is created, the assoc phase is used as the defualt current phase, as opposed to the picked phase

--- src/main/java/gov/usgs/locator/Event.java
+++ src/main/java/gov/usgs/locator/Event.java
@@ -574,13 +574,15 @@ public class Event {
               pickIn.Site.Latitude,
               pickIn.Site.Longitude,
               (pickIn.Site.Elevation / 1000));
+      // when a pick is formed, the associator phase is used as the current phase, not the picked phase which would 
+      // always be set to P
       gov.usgs.locator.Pick pick =
           new gov.usgs.locator.Pick(
               station,
               pickIn.Site.Channel,
               LocUtil.toHydraTime(pickIn.Time.getTime()),
               pickIn.Use,
-              phCode);
+              obsCode);
       pick.setPhaseIdInfo(
           sourceStr,
           pickIn.ID,
